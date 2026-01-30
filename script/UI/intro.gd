cad90d16a1eb89a704a2a75254d10f58dc55dd3c
Panduputran <pamediaid@gmail.com>
2026-01-31 01:49:21 +0700

    Main Menu

--- script/UI/intro.gd
+++ script/UI/intro.gd
@@ -3,35 +3,51 @@ extends Node2D
 @onready var logo_game = $LogoGame
 @onready var logo_ggj = $LogoGGJ
 @onready var logo_sponsor = $LogoSponsor
+@onready var skip_label = $SkipLabel
 
 var next_scene_path = "res://scenes/ui/MainMenu.tscn"
+var can_skip = false
 
 func _ready():
+	skip_label.visible = false
+	skip_label.modulate.a = 0
 	start_intro_sequence()
 
 func _input(event):
-	if event.is_action_pressed("ui_accept") or event.is_action_pressed("ui_cancel"):
+	if can_skip and (event.is_action_pressed("ui_accept") or event.is_action_pressed("ui_select")):
 		change_scene()
 
 func start_intro_sequence():
-	# 1. Mainkan Logo Game
 	play_logo(logo_game)
 	await logo_game.animation_finished
 	await get_tree().create_timer(0.5).timeout
 	logo_game.visible = false
 	
-	# 2. Mainkan Logo GGJ
 	play_logo(logo_ggj)
 	await logo_ggj.animation_finished
 	await get_tree().create_timer(0.5).timeout
 	logo_ggj.visible = false
 	
-	# 3. Mainkan Logo Sponsor
+	show_skip_label()
+	
 	play_logo(logo_sponsor)
 	await logo_sponsor.animation_finished
 	await get_tree().create_timer(0.5).timeout
 	
 	change_scene()
+
+func show_skip_label():
+	can_skip = true
+	skip_label.visible = true
+	
+	var tween = create_tween()
+	tween.tween_property(skip_label, "modulate:a", 1.0, 1.0)
+	tween.tween_callback(loop_blink)
+
+func loop_blink():
+	var tween = create_tween().set_loops()
+	tween.tween_property(skip_label, "modulate:a", 0.2, 0.8)
+	tween.tween_property(skip_label, "modulate:a", 1.0, 0.8)
 
 func play_logo(logo_node):
 	logo_node.visible = true
