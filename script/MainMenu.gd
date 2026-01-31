c0693a58b57a6be010596e9dc5593a91cc68eb8c
Panduputran <pamediaid@gmail.com>
2026-01-31 21:38:00 +0700

    Main Menu

--- script/MainMenu.gd
+++ script/MainMenu.gd
@@ -1,38 +1,60 @@
 extends Control
 
 var button_type = null
+@onready var click: AudioStreamPlayer = $Click
+@onready var hover: AudioStreamPlayer = $Hover
+@onready var bell: AudioStreamPlayer = $Bell
+@onready var music: AudioStreamPlayer = $Music
 
-# Referensi node biar lebih rapi (opsional, tapi disarankan)
 @onready var start_menu_sprite = $StartMenu
 @onready var fade_transition = $Fade_Transition
 @onready var fade_timer = $Fade_Transition/fade_timer
 @onready var fade_anim = $Fade_Transition/AnimationPlayer
 
 func _ready() -> void:
-	pass
+	# Pastikan musik jalan saat menu terbuka (opsional)
+	if music and !music.playing:
+		music.play()
 
+# --- FUNGSI TOMBOL START ---
 func _on_start_pressed() -> void:
 	button_type = "start"
 	
-	$StartMenu.play("default") 
+	# Mainkan suara Bell dan Click khusus untuk Start
+	bell.play()
+	click.play()
+	
+	start_menu_sprite.play("default") 
 	
 	await get_tree().create_timer(3.0).timeout
 	
-	$Fade_Transition.show()
-	$Fade_Transition/fade_timer.start()
-	$Fade_Transition/AnimationPlayer.play("fade_in")
+	fade_transition.show()
+	fade_timer.start()
+	fade_anim.play("fade_in")
 
+# --- FUNGSI TOMBOL LAINNYA ---
+func _on_options_pressed() -> void:
+	click.play()
+	# Tambahkan logika pindah menu opsi di sini
+
+func _on_quit_pressed() -> void:
+	click.play()
+	# Memberi jeda sebentar agar suara klik terdengar sebelum quit
+	await get_tree().create_timer(0.2).timeout
+	get_tree().quit() 
+
+func _on_credit_pressed() -> void:
+	click.play()
+
+func _on_sponsor_pressed() -> void:
+	click.play()
+
+# --- FUNGSI GLOBAL UNTUK HOVER ---
+# Hubungkan sinyal "mouse_entered" dari SETIAP tombol ke fungsi ini
+func _on_button_mouse_entered() -> void:
+	hover.play()
+
+# --- TIMEOUT ---
 func _on_fade_timer_timeout() -> void:
 	if button_type == "start":
 		get_tree().change_scene_to_file("res://scenes/ui/loading.tscn")
-
-func _on_options_pressed() -> void:
-	pass 
-
-func _on_quit_pressed() -> void:
-	get_tree().quit() 
-func _on_credit_pressed() -> void:
-	pass 
-
-func _on_sponsor_pressed() -> void:
-	pass 
--- script/loading.gd
+++ script/loading.gd
@@ -1,23 +1,51 @@
 extends Control
 
 @onready var animation_player = $AnimationPlayer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var count_label = $count 
 
 var progres = []
 var sceneN
 var sceneLS = 0
+var loading_time = 0.0 
+var is_fading = false 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	# 1. Pastikan visual loading terlihat (tidak tertutup ColorRect hitam di awal)
+	# Jika kamu pakai ColorRect untuk fade, set opacity-nya ke 0 dulu
+	# atau pastikan AnimatedSprite2D memiliki Z-index lebih tinggi.
+	
 	sceneN = "res://scenes/ui/CutsceneIntro.tscn"
 	ResourceLoader.load_threaded_request(sceneN)
+	
+	if animated_sprite_2d:
+		animated_sprite_2d.show() # Pastikan tidak tersembunyi
+		animated_sprite_2d.play("default") 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	sceneLS = ResourceLoader.load_threaded_get_status(sceneN,progres)
-	$count.text = str(floor(progres[0]+100)) + "%"
-	await get_tree().create_timer(0.7).timeout
-	if sceneLS == ResourceLoader.THREAD_LOAD_LOADED:
-		var new = ResourceLoader.load_threaded_get(sceneN)
+	loading_time += delta
+	sceneLS = ResourceLoader.load_threaded_get_status(sceneN, progres)
+	
+	# Update teks persentase visual
+	var visual_progress = min((loading_time / 5.0) * 100, 100)
+	if count_label:
+		count_label.text = str(floor(visual_progress)) + "%"
+
+	# Pastikan animasi tetap play (terkadang terhenti jika scene berat)
+	if animated_sprite_2d and not animated_sprite_2d.is_playing():
+		animated_sprite_2d.play("default")
+
+	if sceneLS == ResourceLoader.THREAD_LOAD_LOADED and loading_time >= 5.0 and not is_fading:
+		start_transition_out()
+
+func start_transition_out():
+	is_fading = true
+	
+	if animation_player:
+		# Jika 'fade_in' adalah animasi yang membuat layar jadi HITAM
+		animation_player.play("fade_in") 
+		await animation_player.animation_finished 
+	
+	var new = ResourceLoader.load_threaded_get(sceneN)
+	if new:
 		get_tree().change_scene_to_packed(new)
-		animation_player.play("fade_in")
